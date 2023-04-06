f133e3536249a543846bce7e810895e22a776889
Ryo Martin <laetuzg@gmail.com>
2023-04-06 22:55:05 +0700

    Ignoring SSL certification error.

--- app/src/main/java/com/neotica/neoticawebview/MainActivity.kt
+++ app/src/main/java/com/neotica/neoticawebview/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.neotica.neoticawebview
 
+import android.annotation.SuppressLint
+import android.net.http.SslError
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.SslErrorHandler
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.neotica.neoticawebview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +15,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        bindingWeb()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun bindingWeb(){
+        binding.apply {
+            webView.loadUrl("https://www.neotica.id")
+            webView.webViewClient = MyWebViewClient()
+            webView.settings.javaScriptEnabled = true
+        }
+    }
+}
+
+class MyWebViewClient : WebViewClient() {
+    override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
+        handler?.proceed() // Ignore SSL certificate errors
     }
 }
